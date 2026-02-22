d8aa8bd0d0ed5932b08032afcaa2cfc2358e26e0
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-02-22 17:23:28 +0300

    Добавлена секция Services-cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from "./components/Header.tsx";
 import Hero from "./components/Hero";
 import Services from "./components/Services.tsx";
+import ServicesCards from "./components/Services-cards.tsx";
 
 
 
@@ -11,6 +12,7 @@ function App() {
         <Header/>
         <Hero/>
         <Services/>
+        <ServicesCards/>
     </>
   )
 }
