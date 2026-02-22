36b5f3ced2a051c9beecb8fcb1e5041ebe5820be
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-02-22 16:14:30 +0300

    Реализована секция заголовка Services

---
+++ src/components/Services.tsx
@@ -0,0 +1,9 @@
+export default function Services(){
+    return(
+        <section className="flex flex-col items-center justify-center gap-[13px] mt-[100px] max-md:gap-[8px]">
+            <img src = "/images/services-image.svg" alt = "Service Image" className = "max-w-[160px] max-h-[19px]"/>
+            <h1 className="font-['Source-Serif-4'] font-normal text-[2rem] leading-[38.4px] text-[#3A5F56]">Services</h1>
+            <h2 className = "font-['Roboto] font-normal text-base leading-[22.4px] text-[#292C32] max-w-[591px] text-center max-md:text-sm max-md:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</h2>
+        </section>
+    );
+}
